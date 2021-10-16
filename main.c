f6481bd96bb3f1f68fed90b8e97344f9785b658b
user11024 <user11024@mail.ru>
2021-10-16 19:08:20 +0300

    Added number generation

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-	printf("Hello, World!\n");
+	srand(time(0));
+	int n = rand() % 100;
+	for (int i = 0; i < n; i++)
+	{
+		printf("%5d", rand() % 100);
+	}
 
 	return 0;
 }

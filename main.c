1b62d3a95398e9fe2fcaaa38e16ce077d46a6a5b
user11024 <user11024@mail.ru>
2021-10-16 19:15:08 +0300

    Revert "Added number generation"
    This reverts commit f6481bd96bb3f1f68fed90b8e97344f9785b658b.

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 int main()
 {
-	srand(time(0));
-	int n = rand() % 100;
-	for (int i = 0; i < n; i++)
-	{
-		printf("%5d", rand() % 100);
-	}
+	printf("Hello, World!\n");
 
 	return 0;
 }
